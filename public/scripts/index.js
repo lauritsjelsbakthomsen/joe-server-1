9e7933c3f0049a27f57debd0eea7b5c87c60ba34
lauritsjelsbakthomsen <laurits@jelsbak-thomsen.dk>
2024-10-08 08:29:31 +0200

    Update index.js

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -8,10 +8,8 @@ console.log(responeParagraph);
 
 console.log(btn);
 
-async function fetchData() {
+async function fetchData(url) {
   try {
-    let url = "/res";
-
     let response = await fetch(url);
 
     if (!response.ok) {
@@ -28,7 +26,54 @@ async function fetchData() {
   }
 }
 
+async function fetchLocation() {
+  try {
+    const locationName = "Copenhagen";
+    let url = `https://nominatim.openstreetmap.org/search?q=${locationName}&format=json&addressdetails=1
+`;
+
+    let response = await fetch(url);
+
+    if (!response.ok) {
+      throw error;
+    }
+    let data = await response.json();
+
+    console.log(data[0]);
+
+    console.log(`Lon: ${data[0].lon}`);
+
+    let long = data[0].lon;
+
+    console.log(`Lat: ${data[0].lat}`);
+
+    let lat = data[0].lat;
+
+    fetchWeather(long, lat);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function fetchWeather(long, lat) {
+  try {
+    let url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&current=temperature_2m,is_day,rain,snowfall&hourly=temperature_2m,rain,surface_pressure&daily=sunrise,sunset,daylight_duration,sunshine_duration,uv_index_max,rain_sum,shortwave_radiation_sum`;
+
+    let response = await fetch(url);
+
+    if (!response.ok) {
+      throw error;
+    }
+    let data = await response.json();
+
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 btn.addEventListener("click", function () {
   console.log("click");
-  fetchData();
+  fetchData("/res");
+  fetchLocation();
 });
