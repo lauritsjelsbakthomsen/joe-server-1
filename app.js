79b22fa5a392a180f17dc29f67cdccf8b24e1062
lauritsjelsbakthomsen <laurits@jelsbak-thomsen.dk>
2024-10-08 12:04:06 +0200

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.get("/res", (req, res) => {
 app.post("/cookie", (req, res) => {
   console.log(req.body.location);
 
-  res.cookie("location", req.body.location, { maxAge: 2000 });
+  res.cookie("location", req.body.location, { maxAge: 200000 });
 
   res.status(200).send("Cookie has been made");
 });
